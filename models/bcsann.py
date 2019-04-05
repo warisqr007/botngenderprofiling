4c40ba034a792a079e70a6ce64c240d7bc485846
Waris Quamer <mr.warisquamer@gmail.com>
2019-04-06 02:54:21 +0530

    Update bcsann.py

--- models/bcsann.py
+++ models/bcsann.py
@@ -1,6 +1,8 @@
 from layers.convolution import cnn_layers
 from layers.losses import mse
 from layers.similarity import manhattan_similarity
+from layers.attention import scaled_dot_product_attention
+
 from models.base_model import BaseSiameseNet
 from utils.config_helpers import parse_list
 from layers.basics import dropout
@@ -9,7 +11,7 @@ import numpy as np
 
 _conv_projection_size = 64
 _attention_output_size = 200
-_comparison_output_size = 120
+_comparison_output_size = 100
 
 class AttentionSCnn(BaseSiameseNet):
 
@@ -31,24 +33,13 @@ class AttentionSCnn(BaseSiameseNet):
             pad = tf.zeros([tf.shape(self.x1)[0], 1, self.embedding_size])
             return tf.concat([pad, values, pad], axis=1)
         
-
-    def _attention_layer(self):
-        with tf.name_scope('attention_layer'):
-            e_X1 = tf.layers.dense(self._X1_conv, _attention_output_size, activation=tf.nn.relu, name='attention_nn')
-            e_X2=tf.layers.dense(self._X2_conv, _attention_output_size, activation=tf.nn.relu,
-                                 name='attention_nn',reuse=True)
-            
-            e = tf.matmul(e_X1, e_X2, transpose_b=True, name='e')
-            
-            self._beta = tf.matmul(self._masked_softmax(e, sequence_len), self._X2_conv, name='beta2')
-            self._alpha = tf.matmul(self._masked_softmax(tf.transpose(e, [0,2,1]), sequence_len), self._X1_conv, name='alpha2')
             
     def siamese_layer(self, sequence_len, model_cfg):
         _conv_filter_size = 3
         #parse_list(model_cfg['PARAMS']['filter_sizes'])
         with tf.name_scope('convolutional_layer'):
-            X1_conv_1 = tf.layers.conv1d(
-                self._conv_pad(self.embedded_x1),
+            X_conv_1 = tf.layers.conv1d(
+                self._conv_pad(self.embedded_x),
                 _conv_projection_size,
                 _conv_filter_size,
                 padding='valid',
@@ -56,21 +47,11 @@ class AttentionSCnn(BaseSiameseNet):
                 name='conv_1',
             )
             
-            X2_conv_1 = tf.layers.conv1d(
-                self._conv_pad(self.embedded_x2),
-                _conv_projection_size,
-                _conv_filter_size,
-                padding='valid',
-                use_bias=False,
-                name='conv_1',
-                reuse=True
-            )
             
-            X1_conv_1 = tf.layers.dropout(X1_conv_1, rate=self.dropout, training=self.is_training)
-            X2_conv_1 = tf.layers.dropout(X2_conv_1, rate=self.dropout, training=self.is_training)
+            X_conv_1 = tf.layers.dropout(X_conv_1, rate=self.dropout, training=self.is_training)
             
-            X1_conv_2 = tf.layers.conv1d(
-                self._conv_pad(X1_conv_1),
+            X_conv_2 = tf.layers.conv1d(
+                self._conv_pad(X_conv_1),
                 _conv_projection_size,
                 _conv_filter_size,
                 padding='valid',
@@ -78,82 +59,38 @@ class AttentionSCnn(BaseSiameseNet):
                 name='conv_2',
             )
             
-            X2_conv_2 = tf.layers.conv1d(
-                self._conv_pad(X2_conv_1),
-                _conv_projection_size,
-                _conv_filter_size,
-                padding='valid',
-                use_bias=False,
-                name='conv_2',
-                reuse=True
-            )
+            self._X_conv = tf.layers.dropout(X_conv_2, rate=self.dropout, training=self.is_training)
             
-            self._X1_conv = tf.layers.dropout(X1_conv_2, rate=self.dropout, training=self.is_training)
-            self._X2_conv = tf.layers.dropout(X2_conv_2, rate=self.dropout, training=self.is_training)
+        
             
         with tf.name_scope('attention_layer'):
-            e_X1 = tf.layers.dense(self._X1_conv, _attention_output_size, activation=tf.nn.relu, name='attention_nn')
-            
-            e_X2 = tf.layers.dense(self._X2_conv, _attention_output_size, activation=tf.nn.relu, name='attention_nn', reuse=True)
-            
-            e = tf.matmul(e_X1, e_X2, transpose_b=True, name='e')
-            
-            self._beta = tf.matmul(self._masked_softmax(e, sequence_len), self._X2_conv, name='beta2')
-            self._alpha = tf.matmul(self._masked_softmax(tf.transpose(e, [0,2,1]), sequence_len), self._X1_conv, name='alpha2')
-            
-        with tf.name_scope('self_attention1'):
-            e_X1 = tf.layers.dense(self._X1_conv, _attention_output_size, activation=tf.nn.relu, name='attention_nn1')
-            
-            e_X2 = tf.layers.dense(self._X1_conv, _attention_output_size, activation=tf.nn.relu, name='attention_nn1', reuse=True)
-            
-            e = tf.matmul(e_X1, e_X2, transpose_b=True, name='e')
-            
-            self._beta1 = tf.matmul(self._masked_softmax(e, sequence_len), self._X1_conv, name='beta2') 
-            
-        with tf.name_scope('self_attention2'):
-            e_X1 = tf.layers.dense(self._X2_conv, _attention_output_size, activation=tf.nn.relu, name='attention_nn2')
-            
-            e_X2 = tf.layers.dense(self._X2_conv, _attention_output_size, activation=tf.nn.relu, name='attention_nn2', reuse=True)
-            
-            e = tf.matmul(e_X1, e_X2, transpose_b=True, name='e')
-            
-            self._alpha1 = tf.matmul(self._masked_softmax(e, sequence_len), self._X2_conv, name='beta2')
+            num_blocks = model_cfg['PARAMS'].getint('num_blocks')
+            num_heads = model_cfg['PARAMS'].getint('num_heads')
+            use_residual = model_cfg['PARAMS'].getboolean('use_residual')
+            self.out, self.debug = stacked_multihead_attention(self._X_conv,
+                                                       num_blocks=num_blocks,
+                                                       num_heads=num_heads,
+                                                       use_residual=use_residual,
+                                                       is_training=self.is_training)
             
         with tf.name_scope('comparison_layer'):
-            X1_comp = tf.layers.dense(
-                tf.concat([self._X1_conv, self._beta, self._beta1], 2),
+            X_comp = tf.layers.dense(
+                tf.concat([self._X_conv, self.out], 2),
                 _comparison_output_size,
                 activation=tf.nn.relu,
                 name='comparison_nn'
             )
-            self._X1_comp = tf.multiply(
-                tf.layers.dropout(X1_comp, rate=self.dropout, training=self.is_training),
+            
+            self._X_comp = tf.multiply(
+                tf.layers.dropout(X_comp, rate=self.dropout, training=self.is_training),
                 tf.expand_dims(tf.sequence_mask(sequence_len, tf.reduce_max(sequence_len), dtype=tf.float32), -1)
             )
             
-            X2_comp = tf.layers.dense(
-                tf.concat([self._X2_conv, self._alpha,self._alpha1], 2),
-                _comparison_output_size,
-                activation=tf.nn.relu,
-                name='comparison_nn',
-                reuse=True
-            )
-            self._X2_comp = tf.multiply(
-                tf.layers.dropout(X2_comp, rate=self.dropout, training=self.is_training),
-                tf.expand_dims(tf.sequence_mask(sequence_len, tf.reduce_max(sequence_len), dtype=tf.float32), -1)
-            )
+            X_agg = tf.reduce_sum(self._X_comp, 1)
         
-            X1_agg = tf.reduce_sum(self._X1_comp, 1)
-            X2_agg = tf.reduce_sum(self._X2_comp, 1)
-            
-            self._agg = tf.concat([X1_agg, X2_agg], 1)
-        
-        return manhattan_similarity(X1_agg,X2_agg)
-        '''
         with tf.name_scope('classifier'):
             L1 = tf.layers.dropout(
-                tf.layers.dense(self._agg, 100, activation=tf.nn.relu, name='L1'),
+                tf.layers.dense(self.X_agg, 100, activation=tf.nn.relu, name='L1'),
                 rate=self.dropout, training=self.is_training)
             y = tf.layers.dense(L1, 1, activation=tf.nn.softmax, name='y')
         return y
-        '''
