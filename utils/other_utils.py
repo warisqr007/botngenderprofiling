1e626d3bc20c41c1507acb47abb66afaa7791765
Waris Quamer <mr.warisquamer@gmail.com>
2019-04-06 01:28:46 +0530

    Update other_utils.py

--- utils/other_utils.py
+++ utils/other_utils.py
@@ -14,14 +14,13 @@ def timer(start, end):
     return "{:0>2}:{:0>2}:{:05.2f}".format(int(hours), int(minutes), seconds)
 
 
-def evaluate_model(model, session, x1, x2, labels, batch_size=100):
-    batch_helper = BatchHelper(x1, x2, labels, batch_size)
-    num_batches = len(x1) // batch_size
+def evaluate_model(model, session, x, labels, batch_size=100):
+    batch_helper = BatchHelper(x, labels, batch_size)
+    num_batches = len(x) // batch_size
     accuracy = 0.0
     for batch in range(num_batches):
-        x1_batch, x2_batch, y_batch = batch_helper.next(batch)
-        feed_dict = {model.x1: x1_batch,
-                     model.x2: x2_batch,
+        x_batch, y_batch = batch_helper.next(batch)
+        feed_dict = {model.x: x_batch,
                      model.is_training: False,
                      model.labels: y_batch}
         accuracy += session.run(model.accuracy, feed_dict=feed_dict)
