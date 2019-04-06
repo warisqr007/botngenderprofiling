7f04965e6cc18f67bf858eb6f2cdb1fd3a8c75bb
Waris Quamer <mr.warisquamer@gmail.com>
2019-04-06 16:52:36 +0530

    Update prepare_data.sh

--- bin/prepare_data.sh
+++ bin/prepare_data.sh
@@ -10,18 +10,16 @@ GENDER_TRAIN_ID=1XdDRLCprBPmu-dXZKAzTrl6V2OIcXFoX
 GENDER_DEV_ID=1pXy9qD7serdcQzbr9yJgXGytyHf2tflM
 
 
-https://drive.google.com/uc?id=1unxuYa3-6ZrS4W1HLeg0gfqzIYHLVNN1&export=download
-
-
 SNLI_DATA=${SNLI_DATA:-https://drive.google.com/uc?export=download&id=${SNLI_ID}}
 QQP_DATA_TRAIN=${QQP_DATA_TRAIN:-https://drive.google.com/uc?export=download&id=${QQP_TRAIN_ID}}
 QQP_DATA_TEST=${QQP_DATA_TEST:-https://drive.google.com/uc?export=download&id=${QQP_TEST_ID}}
 
 
-BOT_TRAIN_DATA=${BOT_TRAINLINK:-https://drive.google.com/file/d/1unxuYa3-6ZrS4W1HLeg0gfqzIYHLVNN1/view?usp=sharing}
-BOT_DEV_DATA=${BOT_DEVLINK:-https://drive.google.com/file/d/1EtcJmW4qattUEm1RZ8hUfIbCQVViVo53/view?usp=sharing}
-GENDER_DEV_DATA=${GENDER_DEVLINK:-https://drive.google.com/file/d/1pXy9qD7serdcQzbr9yJgXGytyHf2tflM/view?usp=sharing}
-GENDER_TRAIN_DATA=${GENDER_TRAINLINK:-https://drive.google.com/file/d/1XdDRLCprBPmu-dXZKAzTrl6V2OIcXFoX/view?usp=sharing}
+BOT_TRAIN_DATA=${BOT_TRAIN_DATA:-https://drive.google.com/uc?export=download&id=${BOT_TRAIN_ID}}
+BOT_DEV_DATA=${BOT_DEV_DATA:-https://drive.google.com/uc?export=download&id=${BOT_DEV_ID}}
+GENDER_TRAIN_DATA=${GENDER_TRAIN_DATA:-https://drive.google.com/uc?export=download&id=${GENDER_TRAIN_ID}}
+GENDER_DEV_DATA=${GENDER_DEV_DATA:-https://drive.google.com/uc?export=download&id=${GENDER_DEV_ID}}
+
 
 BOT_DIR=Bot
 GENDER_DIR=Gender
@@ -35,10 +33,10 @@ SNLI_FILE=train_snli.tgz
 QQP_FILE_TRAIN=qqp_train.tgz
 QQP_FILE_TEST=qqp_test.tgz
 
-BOT_TRAINFILE=dataTrainBot.csv
-BOT_DEVFILE=dataDevBot.csv
-GENDER_TRAINFILE=dataTrainGender.csv
-GENDER_DEVFILE=dataDevGender.csv
+BOT_TRAIN_FILE=dataTrainBot.csv
+BOT_DEV_FILE=dataDevBot.csv
+GENDER_TRAIN_FILE=dataTrainGender.csv
+GENDER_DEV_FILE=dataDevGender.csv
 
 mkdir ../${CORPORA_DIR}
 cd ../${CORPORA_DIR}
@@ -54,12 +52,10 @@ function google_drive_big_file_download () {
 #wget --no-check-certificate ${QQP_DATA_TRAIN} -O ${QQP_DIR}/${QQP_FILE_TRAIN}
 #google_drive_big_file_download ${QQP_TEST_ID} ${QQP_DIR}/${QQP_FILE_TEST}
 
-wget ${BOT_TRAINLINK} -O ${BOT_DIR}/${BOT_TRAINFILE}
-wget ${BOT_DEVLINK} -O ${BOT_DIR}/${BOT_DEVFILE}
-wget ${GENDER_TRAINLINK} -O ${GENDER_DIR}/${GENDER_TRAINFILE}
-wget ${GENDER_DEVLINK} -O ${GENDER_DIR}/${GENDER_DEVFILE}
-
-
+wget --no-check-certificate ${BOT_TRAIN_DATA} -O ${BOT_DIR}/${BOT_TRAIN_FILE}
+wget --no-check-certificate ${BOT_DEV_DATA} -O ${BOT_DIR}/${BOT_DEV_FILE}
+wget --no-check-certificate ${GENDER_TRAIN_DATA} -O ${GENDER_DIR}/${GENDER_TRAIN_FILE}
+wget --no-check-certificate ${GENDER_DEV_DATA} -O ${GENDER_DIR}/${GENDER_DEV_FILE}
 
 
 
