f69b6aaaaca2deeb3e7f8148e302855f7409e9ed
Waris Quamer <mr.warisquamer@gmail.com>
2019-04-06 17:08:29 +0530

    Update run.py

--- run.py
+++ run.py
@@ -23,17 +23,17 @@ def train(main_config, model_config, model_name, dataset_name):
     model_name = '{}_{}'.format(model_name,
                                 main_config['PARAMS']['embedding_size'])
 
-    train_data = dataset.train_set_pairs()
+    train_data = dataset.train_set_text()
     vectorizer = DatasetVectorizer(train_data, main_cfg.model_dir)
 
     dataset_helper = Dataset(vectorizer, dataset, main_cfg.batch_size)
     max_sentence_len = vectorizer.max_sentence_len
     vocabulary_size = vectorizer.vocabulary_size
 
-    train_mini_sen1, train_mini_sen2, train_mini_labels = dataset_helper.pick_train_mini_batch()
+    train_mini_sen, train_mini_labels = dataset_helper.pick_train_mini_batch()
     train_mini_labels = train_mini_labels.reshape(-1, 1)
 
-    test_sentence1, test_sentence2 = dataset_helper.test_instances()
+    test_sentence = dataset_helper.test_instances()
     test_labels = dataset_helper.test_labels()
     test_labels = test_labels.reshape(-1, 1)
 
@@ -54,28 +54,26 @@ def train(main_config, model_config, model_name, dataset_name):
         for epoch in tqdm(range(main_cfg.num_epochs), desc='Epochs'):
             start_time = time.time()
 
-            train_sentence1, train_sentence2 = dataset_helper.train_instances(shuffle=True)
+            train_sentence = dataset_helper.train_instances(shuffle=True)
             train_labels = dataset_helper.train_labels()
 
-            train_batch_helper = BatchHelper(train_sentence1, train_sentence2, train_labels, main_cfg.batch_size)
+            train_batch_helper = BatchHelper(train_sentence, train_labels, main_cfg.batch_size)
 
             # small eval set for measuring dev accuracy
-            dev_sentence1, dev_sentence2, dev_labels = dataset_helper.dev_instances()
+            dev_sentence, dev_labels = dataset_helper.dev_instances()
             dev_labels = dev_labels.reshape(-1, 1)
 
             tqdm_iter = tqdm(range(num_batches), total=num_batches, desc="Batches", leave=False, postfix=metrics)
             for batch in tqdm_iter:
                 global_step += 1
-                sentence1_batch, sentence2_batch, labels_batch = train_batch_helper.next(batch)
-                feed_dict_train = {model.x1: sentence1_batch,
-                                   model.x2: sentence2_batch,
+                sentence_batch, labels_batch = train_batch_helper.next(batch)
+                feed_dict_train = {model.x: sentence_batch,
                                    model.is_training: True,
                                    model.labels: labels_batch}
                 loss, _ = session.run([model.loss, model.opt], feed_dict=feed_dict_train)
 
                 if batch % main_cfg.eval_every == 0:
-                    feed_dict_train = {model.x1: train_mini_sen1,
-                                     model.x2: train_mini_sen2,
+                    feed_dict_train = {model.x: train_mini_sen,
                                      model.is_training: False,
                                      model.labels: train_mini_labels}
 
@@ -83,8 +81,7 @@ def train(main_config, model_config, model_name, dataset_name):
                                                                 feed_dict=feed_dict_train)
                     log_saver.log_train(train_summary, global_step)
 
-                    feed_dict_dev = {model.x1: dev_sentence1,
-                                     model.x2: dev_sentence2,
+                    feed_dict_dev = {model.x: dev_sentence,
                                      model.is_training: False,
                                      model.labels: dev_labels}
 
@@ -100,7 +97,7 @@ def train(main_config, model_config, model_name, dataset_name):
                 if global_step % main_cfg.save_every == 0:
                     model_saver.save(session, global_step=global_step)
 
-            model_evaluator.evaluate_dev(dev_sentence1, dev_sentence2, dev_labels)
+            model_evaluator.evaluate_dev(dev_sentence, dev_labels)
 
             end_time = time.time()
             total_time = timer(start_time, end_time)
@@ -108,7 +105,7 @@ def train(main_config, model_config, model_name, dataset_name):
 
             model_saver.save(session, global_step=global_step)
 
-        model_evaluator.evaluate_test(test_sentence1, test_sentence2, test_labels)
+        model_evaluator.evaluate_test(test_sentence, test_labels)
         model_evaluator.save_evaluation('{}/{}'.format(main_cfg.model_dir, model_name), time_per_epoch[-1], dataset)
 
 
@@ -131,12 +128,10 @@ def predict(main_config, model_config, model):
         last_checkpoint = tf.train.latest_checkpoint('{}/{}/model'.format(model_dir, model_name))
         saver.restore(session, last_checkpoint)
         while True:
-            x1 = input('First sentence:')
-            x2 = input('Second sentence:')
-            x1_sen = vectorizer.vectorize(x1)
-            x2_sen = vectorizer.vectorize(x2)
+            x = input('Text:')
+            x_sen = vectorizer.vectorize(x)
 
-            feed_dict = {model.x1: x1_sen, model.x2: x2_sen}
+            feed_dict = {model.x: x_sen}
             prediction = session.run([model.temp_sim], feed_dict=feed_dict)
             print(prediction)
 
@@ -153,7 +148,7 @@ def main():
                         help='model to be used')
 
     parser.add_argument('dataset',
-                        choices=['QQP', 'SNLI'],
+                        choices=['Gender', 'Bot'],
                         help='dataset to be used')
 
     parser.add_argument('--gpu',
